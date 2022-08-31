88c588d48a88b887a72c974fbbb6194a1a9262a3
pi <1953812182@qq.com>
2022-08-31 13:21:35 +1000

    added micheal's code

--- pwmTest.py
+++ pwmTest.py
@@ -5,23 +5,29 @@ http://www.electronicwings.com
 
 import gpiozero
 import time
-ledpin = 12
-pwm = gpiozero.PWMOutputDevice(pin=ledpin,active_high=True,initial_value=0,frequency=100)
-dir1 = gpiozero.OutputDevice(pin=4)
-dir2 = gpiozero.OutputDevice(pin=17)
-
-encoder = gpiozero.RotaryEncoder(a=21, b=20,max_steps=100000) 
+#ledpin = 12
+pwm = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=100)
+dir1 = gpiozero.OutputDevice(pin=5)
+dir2 = gpiozero.OutputDevice(pin=6)
+dir1.value=0
+dir2.value=1
+#encoder = gpiozero.RotaryEncoder(a=21, b=20,max_steps=100000) 
 # This class has a lot more functionality,so worth reading up on it
-# Step through duty cycle values, slowly increasing the speed and changing the direction of motion
-encoder.steps = 0
-while 1:
-    pwm.value = 0.2
+# Step through duty cycle values, slowly increasing the speed and changing the direction of motion#
+#encoder.steps = 0
+for _ in range(10):
+    for i in range(1,11):
+        pwm.value = i*0.1
+        time.sleep(1)
+        print('Duty cycle:',pwm.value)
+        #print('Counter:',encoder.steps,'Speed:',(encoder.steps)/5.0,'steps per second\n')
+        #encoder.steps = 0
+    
+    print('Direction:',dir1.value)
     dir1.value = not dir1.value
     dir2.value = not dir1.value
-    print('Duty cycle:',pwm.value,'Direction:',dir1.value)
-    time.sleep(5.0)
-    print('Counter:',encoder.steps,'Speed:',(encoder.steps)/5.0,'steps per second\n')
-    encoder.steps = 0
+    
+        
 
 pwm.value =0 
 pwm.off()
